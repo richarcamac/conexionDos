0e8931d5b9697ca99e1275db07d5ccbf059cc3fa
richarcamac <richi.rcp42@gmail.com>
2025-09-30 00:33:09 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 
-class Conexion{
+class Conexion {
 
-	static public function conectar(){
+    static public function conectar() {
 
-		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-		
-		$link = new PDO("mysql:host=mysql-asiridata.alwaysdata.net;dbname=asiridata_usuarios",
-						"asiridata",
-						"123456789Asiri", $options);
+        $options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+        );
 
-		$link->exec("set names utf8");
+        $link = new PDO(
+            "mysql:host=mysql-asiridata.alwaysdata.net;dbname=asiridata_usuarios",
+            "asiridata",
+            "123456789Asiri",
+            $options
+        );
 
-		$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$link->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-		$link->setAttribute(PDO::MYSQL_ATTR_LOCAL_INFILE, 1);
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $link->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $link->setAttribute(PDO::MYSQL_ATTR_LOCAL_INFILE, 1);
 
-		return $link;
-
-	}
-
+        return $link;
+    }
 }
+
